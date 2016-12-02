adcf0f87b9fb82499fcf2af862c8d965356f212c
Thomas Hudson <g.t.hudson@durham.ac.uk>
2016-12-02 16:31:09 +0000

    Patent code

--- pubPatent.py
+++ pubPatent.py
@@ -1,13 +1,27 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 
-sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+sparql = SPARQLWrapper("http://gov.tso.co.uk/patents/sparql")
 sparql.setQuery("""
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    SELECT ?label
-    WHERE { <http://dbpedia.org/resource/Asturias> rdfs:label ?label }
+    PREFIX research: <http://research.data.gov.uk/def/project/>
+PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+PREFIX patents: <http://patents.data.gov.uk/def/patents/>
+PREFIX dcterms: <http://purl.org/dc/terms/>
+SELECT ?patents  ?filingDate ?postcode ?long ?lat ?subject WHERE {
+ 	  ?patents patents:applicant ?applicant ;
+	   dcterms:subject ?subject ;
+	   patents:filingDate ?filingDate .
+	   ?applicant research:location ?location .
+	   ?location research:postcode ?postcode ;
+	   geo:long ?long ;
+	   geo:lat ?lat .
+}
 """)
 sparql.setReturnFormat(JSON)
 results = sparql.query().convert()
 
+latLongs = []
+
 for result in results["results"]["bindings"]:
-    print(result["label"]["value"])
+    latLongs.append((result["lat"]["value"],result["long"]["value"]))
+
+print(latLongs)
